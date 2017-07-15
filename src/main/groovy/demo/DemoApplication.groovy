df21167071c8bc23474f50c71d22b0f74b5f3728
Bryan Hughes <hugheba@gmail.com>
2017-07-15 12:47:14 -0400

    Added freemarker and javamelody

--- src/main/groovy/demo/DemoApplication.groovy
+++ src/main/groovy/demo/DemoApplication.groovy
@@ -3,6 +3,7 @@ package demo
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
@@ -10,9 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody
 @SpringBootApplication
 class DemoApplication {
     @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        "Hello World!"
+    String home(Model model) {
+        "home"
     }
 
     static void main(String[] args) {
